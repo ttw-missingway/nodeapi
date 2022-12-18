3d933d1b1f373dbdb47ffa6f827002a12b2b7092
Dylan Walling <dylanwalling@gmail.com>
2022-12-17 19:09:34 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// briung in routes
+const postRoutes = require('./routes/post');
+
+const myOwnMiddleware = (req, res, next) => {
+    console.log("middleware applied!!!");
+    next();
+};
+
+//middleware
+app.use(morgan('dev'));
+app.use(myOwnMiddleware);
+app.use("/", postRoutes);
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Node Js API is listening on port: ${port}`);
+});
