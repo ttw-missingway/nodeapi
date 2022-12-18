314758205bc799f87028ed358e0f9801ab0cdb23
Dylan Walling <dylanwalling@gmail.com>
2022-12-17 20:24:14 -0600

    post method

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -21,6 +22,7 @@ const postRoutes = require('./routes/post');
 //middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(expressValidator());
 app.use("/", postRoutes);
 
 
--- controllers/post.js
+++ controllers/post.js
@@ -12,13 +12,9 @@ exports.getPosts = (req, res) => {
 
 exports.createPost = (req, res) => {
     const post = new Post(req.body);
-    // console.log("CREATING POST: ", req.body);
-    post.save((err, result) => {
-        if (err) {
-            return res.status(400).json({
-                error: err
-            });
-        }
+
+    post.save()
+    .then(result => {
         res.status(200).json({
             post: result
         });
