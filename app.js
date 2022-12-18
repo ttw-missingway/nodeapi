f4578e756630c01c5964d6f9c8af2fd85b028e39
Dylan Walling <dylanwalling@gmail.com>
2022-12-17 19:58:07 -0600

    connected to mongoDB

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// briung in routes
+//db
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
+
+// bring in routes
 const postRoutes = require('./routes/post');
-
-const myOwnMiddleware = (req, res, next) => {
-    console.log("middleware applied!!!");
-    next();
-};
 
 //middleware
 app.use(morgan('dev'));
-app.use(myOwnMiddleware);
+app.use(bodyParser.json());
 app.use("/", postRoutes);
 
-const port = 8080;
 
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Node Js API is listening on port: ${port}`);
 });
