a03ed337d1acdec7fb517c26422c4ea01c34875e
Dylan Walling <dylanwalling@gmail.com>
2022-12-18 16:30:14 -0600

    nav menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from './components/nav/Menu';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>
-        This is App Component
-      </h1>
-    </div>
+    <BrowserRouter>
+    <Menu/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ client/src/pages/Register.js
@@ -0,0 +1,9 @@
+export default function Register() {
+    return (
+      <div>
+        <h1>
+          This is Register Page
+        </h1>
+      </div>
+    );
+  }
